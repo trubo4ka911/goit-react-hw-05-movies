62ceb893afda068d29c7834bd9ea1bdc65094931
Anna Trubnikova <trubo4ka911@gmail.com>
2022-02-15 11:32:27 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Navigation from "./components/Navigation/Navigation";
 
 const HomePage = lazy(() => import("./views/HomePage"));
 const MovieDetailView = lazy(() => import("./views/MovieDetailView"));
-const Credit = lazy(() => import("./components/Credit/MovieCreditView"));
 const Searchbar = lazy(() => import("./components/Searchbar/Searchbar"));
-const Review = lazy(() => import("./components/Reviews/MovieReviewView"));
 
 function App() {
   return (
@@ -17,23 +15,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/movies" exact component={Searchbar} />
-
-          {/* <Route path="/movies/:movieId" exact component={MovieDetailView} >
-  <Route path='cast' element={<Credit />}/>
-  <Route path='reviews' element={<Review />}/>
-</Route> */}
-
           <Route path="/movies/:movieId" exact component={MovieDetailView} />
-          <Route path="/movies/:movieId/cast" exact>
-            <MovieDetailView>
-              <Credit />
-            </MovieDetailView>
-          </Route>
-          <Route path="/movies/:movieId/reviews">
-            <MovieDetailView>
-              <Review />
-            </MovieDetailView>
-          </Route>
         </Switch>
       </Suspense>
     </Container>
--- src/views/MovieDetailView.jsx
+++ src/views/MovieDetailView.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { Link, useParams, useHistory, useLocation } from "react-router-dom";
+import { useState, useEffect, Suspense, lazy } from "react";
+import {
+  Link,
+  useParams,
+  useHistory,
+  useLocation,
+  useRouteMatch,
+  Switch,
+  Route,
+} from "react-router-dom";
 import { getIdMovies, getCast } from "../Api/moviesApi";
 import PageHeading from "../components/PageHeading/PageHeading";
 import { toast } from "react-toastify";
 import styles from "./views.module.css";
+
+const Credit = lazy(() => import("../components/Credit/MovieCreditView"));
+const Review = lazy(() => import("../components/Reviews/MovieReviewView"));
 
 const MovieDetailView = (props) => {
   const [movie, setMovie] = useState("");
   const { movieId } = useParams();
   const history = useHistory();
   const location = useLocation();
+  const { path, url } = useRouteMatch();
 
   useEffect(() => {
     getIdMovies(movieId).then(setMovie).catch(toast.error("Not found!"));
@@ -18,48 +30,55 @@ const MovieDetailView = (props) => {
 
   const onGoBack = () => {
     history.goBack();
-    // history.push(location.state?.from ?? "/")
   };
-
+  console.log(path);
+  console.log(url);
   return (
     <>
-      <PageHeading text={movie.title} />
-      <button className={styles.goBack} type="button" onClick={onGoBack}>
-        Go back
-      </button>
-      <hr />
-      {movie && (
-        <>
-          <div className={styles.content}>
-            <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <div>
-              <h3>Vote average:</h3>
-              <p>{movie.vote_average}</p>
-              <h3>Overview:</h3>
-              <p>{movie.overview}</p>
-              <h3>Genres:</h3>
-              <ul>
-                {movie &&
-                  movie.genres.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-          <h3 className={styles.info}>Additional information:</h3>
-        </>
-      )}
-      <hr />
-      <Link className={styles.contentLink} to={`/movies/${movieId}/cast`}>
-        Cast
-      </Link>
-      <Link className={styles.contentLink} to={`/movies/${movieId}/reviews`}>
-        Reviews
-      </Link>
-      {props.children}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path={path}>
+            <PageHeading text={movie.title} />
+            <button className={styles.goBack} type="button" onClick={onGoBack}>
+              Go back
+            </button>
+            <hr />
+            {movie && (
+              <>
+                <div className={styles.content}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <div>
+                    <h3>Vote average:</h3>
+                    <p>{movie.vote_average}</p>
+                    <h3>Overview:</h3>
+                    <p>{movie.overview}</p>
+                    <h3>Genres:</h3>
+                    <ul>
+                      {movie &&
+                        movie.genres.map((item) => (
+                          <li key={item.id}>{item.name}</li>
+                        ))}
+                    </ul>
+                  </div>
+                </div>
+                <h3 className={styles.info}>Additional information:</h3>
+              </>
+            )}
+            <hr />
+            <Link className={styles.contentLink} to={`${url}/cast`}>
+              Cast
+            </Link>
+            <Link className={styles.contentLink} to={`${url}/reviews`}>
+              Reviews
+            </Link>
+          </Route>
+          <Route path={`${path}/cast`} exact component={Credit} />
+          <Route path={`${path}/reviews`} exact component={Review} />
+        </Switch>
+      </Suspense>
     </>
   );
 };
