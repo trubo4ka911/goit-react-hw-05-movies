34ee0157165e3359f4345bb4cbe57b8085b4796f
Anna Trubnikova <trubo4ka911@gmail.com>
2022-02-16 12:27:36 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Suspense, lazy } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Container from "./components/Container/Container";
 import Navigation from "./components/Navigation/Navigation";
 
 const HomePage = lazy(() => import("./views/HomePage"));
 const MovieDetailView = lazy(() => import("./views/MovieDetailView"));
+const Credit = lazy(() => import("./components/Credit/MovieCreditView"));
 const Searchbar = lazy(() => import("./components/Searchbar/Searchbar"));
+const Review = lazy(() => import("./components/Reviews/MovieReviewView"));
 
 function App() {
   return (
@@ -16,6 +18,33 @@ function App() {
           <Route path="/" exact component={HomePage} />
           <Route path="/movies" exact component={Searchbar} />
           <Route path="/movies/:movieId" exact component={MovieDetailView} />
+
+          {/* <Route path="/movies/:movieId" index element={<MovieDetailView/>} />
+<Route path='cast' element={<Credit/>}/>
+  <Route path='reviews' element={<Review/>}/> */}
+
+          {/* <Route component={MovieDetailView} path="/movies/:movieId">
+            <Route component={Credit} path="/cast"></Route>
+            <Route component={Review} path="/reviews"></Route>
+        </Route> */}
+
+          {/* <Switch>
+<Route path="/movies/:movieId" exact component={MovieDetailView} />
+<Route path="/cast"  component={Credit} />
+<Route path="/reviews"  component={Review} />
+</Switch> */}
+
+          {/* <Route path="/movies/:movieId" exact component={MovieDetailView} />
+          <Redirect path="/movies/:movieId/cast" exact>
+            <MovieDetailView>
+              <Credit />
+            </MovieDetailView>
+          </Redirect>
+          <Redirect path="/movies/:movieId/reviews">
+            <MovieDetailView>
+              <Review />
+            </MovieDetailView>
+          </Redirect> */}
         </Switch>
       </Suspense>
     </Container>
--- src/views/MovieDetailView.jsx
+++ src/views/MovieDetailView.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { useState, useEffect, Suspense, lazy } from "react";
+import { useState, useEffect, lazy } from "react";
 import {
   Link,
   useParams,
   useHistory,
   useLocation,
-  useRouteMatch,
   Switch,
   Route,
+  useRouteMatch,
 } from "react-router-dom";
 import { getIdMovies, getCast } from "../Api/moviesApi";
 import PageHeading from "../components/PageHeading/PageHeading";
 import { toast } from "react-toastify";
 import styles from "./views.module.css";
-
 const Credit = lazy(() => import("../components/Credit/MovieCreditView"));
 const Review = lazy(() => import("../components/Reviews/MovieReviewView"));
 
-const MovieDetailView = (props) => {
+const MovieDetailView = () => {
   const [movie, setMovie] = useState("");
   const { movieId } = useParams();
   const history = useHistory();
@@ -31,54 +30,53 @@ const MovieDetailView = (props) => {
   const onGoBack = () => {
     history.goBack();
   };
-  console.log(path);
-  console.log(url);
+
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path={path}>
-            <PageHeading text={movie.title} />
-            <button className={styles.goBack} type="button" onClick={onGoBack}>
-              Go back
-            </button>
-            <hr />
-            {movie && (
-              <>
-                <div className={styles.content}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <div>
-                    <h3>Vote average:</h3>
-                    <p>{movie.vote_average}</p>
-                    <h3>Overview:</h3>
-                    <p>{movie.overview}</p>
-                    <h3>Genres:</h3>
-                    <ul>
-                      {movie &&
-                        movie.genres.map((item) => (
-                          <li key={item.id}>{item.name}</li>
-                        ))}
-                    </ul>
-                  </div>
-                </div>
-                <h3 className={styles.info}>Additional information:</h3>
-              </>
-            )}
-            <hr />
-            <Link className={styles.contentLink} to={`${url}/cast`}>
-              Cast
-            </Link>
-            <Link className={styles.contentLink} to={`${url}/reviews`}>
-              Reviews
-            </Link>
-          </Route>
-          <Route path={`${path}/cast`} exact component={Credit} />
-          <Route path={`${path}/reviews`} exact component={Review} />
-        </Switch>
-      </Suspense>
+      <PageHeading text={movie.title} />
+      <button className={styles.goBack} type="button" onClick={onGoBack}>
+        Go back
+      </button>
+      <hr />
+      {movie && (
+        <>
+          <div className={styles.content}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div>
+              <h3>Vote average:</h3>
+              <p>{movie.vote_average}</p>
+              <h3>Overview:</h3>
+              <p>{movie.overview}</p>
+              <h3>Genres:</h3>
+              <ul>
+                {movie &&
+                  movie.genres.map((item) => (
+                    <li key={item.id}>{item.name}</li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+          <h3 className={styles.info}>Additional information:</h3>
+        </>
+      )}
+      <hr />
+
+      <Link className={styles.contentLink} to={`${url}/cast`}>
+        Cast
+      </Link>
+      <Link className={styles.contentLink} to={`${url}/reviews`}>
+        Reviews
+      </Link>
+
+      {/* <Switch>
+          <Route path={path} />         
+          <Route path={`${path}/:cast`}  component={Credit} />
+          <Route path={`${path}/:reviews`}  component={Review} />
+          <Route path={`${path}/`} exact component={MovieDetailView} />
+      </Switch> */}
     </>
   );
 };
